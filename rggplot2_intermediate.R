09ff794f810b0d3f72b96f909942ee5c2cee37d1
Pritii Tam Wai Yin <Pritii@rgsenv.local>
2020-03-04 14:26:09 +1000

    intermediate

---
+++ rggplot2_intermediate.R
@@ -0,0 +1,12 @@
+# Description: intermediate session on ggplot2
+# Author: Pritii Tam
+# 2020 - 03 - 04
+ 
+# Import data
+gapminder <- read.csv("https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv")
+
+# ctrl + enter: execute from script
+
+# familiarise yourself with the data
+summary(gapminder)
+View(gapminder)
